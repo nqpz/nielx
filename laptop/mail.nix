a9684086a4245fce26fc2903c1f9d2b0088b183b
Niels G. W. Serup <ngws@metanohi.name>
2020-11-01 18:29:12 +0100

    Add test email sender script

--- laptop/mail.nix
+++ laptop/mail.nix
@@ -37,5 +37,22 @@ else
     ${screen} -S offlineimap ${pkgs.offlineimap}/bin/offlineimap
 fi
 '')
+
+    (pkgs.writeScriptBin "sendmail-test" ''#!/${pkgs.python3}/bin/python3
+import smtplib
+from email.mime.text import MIMEText
+
+def send_email(address_from, address_to, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = address_from
+    msg['To'] = address_to
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+
+if __name__ == '__main__':
+    send_email('test@${cfg.www.domain}', '${cfg.email}', 'Test', 'Test test.')
+'')
   ];
 }
