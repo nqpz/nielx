262e1424adf277efe00f5e505c39b7005ec4378a
Niels G. W. Serup <ngws@metanohi.name>
2020-12-21 11:41:45 +0100

    + wav

--- laptop/bin/outils/mpv.py
+++ laptop/bin/outils/mpv.py
@@ -1,6 +1,6 @@
 from outils import *
 
-_media_endings = ['mkv', 'mp4', 'webm', 'ogg', 'ogv', 'mp3', 'm2t', 'flac', 'avi', 'mpg', 'mts', 'mov']
+_media_endings = ['mkv', 'mp4', 'webm', 'ogg', 'ogv', 'mp3', 'm2t', 'flac', 'avi', 'mpg', 'mts', 'mov', 'wav']
 
 def open(filenames):
     first = filenames[0]
