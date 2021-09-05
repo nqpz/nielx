df9ea3babca56957138da34c5d979b38ce689b06
Niels G. W. Serup <ngws@metanohi.name>
2021-09-05 18:45:56 +0200

    Add roundworm interface

---
+++ nielx/roundworm.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.nielx.roundworm;
+in
+{
+  options.nielx.roundworm = {
+    enable = mkEnableOption "browse media stored in S3";
+
+    config = mkOption {
+      type = types.str;
+    };
+
+    domain = mkOption {
+      type = types.str;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    nielx.services.roundworm = {
+      command = "${config.nielx.root}/nielx/roundworm/roundworm ${cfg.config}";
+      packages = (import ./roundworm/shell.nix).buildInputs;
+      user = config.nielx.user;
+      group = "users";
+    };
+  };
+}
