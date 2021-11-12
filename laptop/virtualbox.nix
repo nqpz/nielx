f6f1b805a35a77e95dd5374a50cdcc92f5962003
Niels G. W. Serup <ngws@metanohi.name>
2021-11-12 19:06:22 +0100

    Enable VirtualBox guest additions

--- laptop/virtualbox.nix
+++ laptop/virtualbox.nix
@@ -7,6 +7,7 @@ in
 {
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
 
   users.extraGroups.vboxusers.members = [ cfg.user ];
 
