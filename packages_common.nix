ab341a3f6dd15d038d4bbf6c2e42e0fabccf24e3
Niels G. W. Serup <ngws@metanohi.name>
2022-07-03 18:32:44 +0200

    Do, not echo

--- packages_common.nix
+++ packages_common.nix
@@ -52,7 +52,7 @@ ${pkgs.niv}/bin/niv update
 
 nixpkgs=$(echo '"${"$" + "{(import ./nix/sources.nix).nixpkgs}"}"' | nix repl 2>/dev/null | tr -d '"')
 
-echo sudo nixos-rebuild switch -I nixpkgs=$nixpkgs -I nixos=$nixpkgs -I nixos-config=/etc/nixos/configuration.nix
+sudo nixos-rebuild switch -I nixpkgs=$nixpkgs -I nixos=$nixpkgs -I nixos-config=/etc/nixos/configuration.nix
 '')
   ];
 }
