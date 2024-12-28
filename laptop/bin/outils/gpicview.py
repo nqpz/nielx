842b7093ef2bcaeece86bb91785f2399985d59ad
Niels G. W. Serup <ngws@metanohi.name>
2024-12-28 14:57:40 +0100

    Use loupe and for more formats

--- laptop/bin/outils/gpicview.py
+++ laptop/bin/outils/gpicview.py
@@ -1,12 +1,12 @@
 from outils import *
 
-_media_endings = ['jpg', 'jpeg', 'png', 'bmp']
+_media_endings = ['jpg', 'jpeg', 'png', 'bmp', 'webp', 'avif']
 
 def open(filenames):
     first = filenames[0]
     ext = get_extension(first)
     if ext in _media_endings:
-        return ['gpicview'] + filenames
+        return ['loupe'] + filenames
     elif ext == 'gif':  # hack: use something else
         return ['animate', '-backdrop', '-loop', '0'] + filenames
     elif ext == 'svg':  # hack: use something else
